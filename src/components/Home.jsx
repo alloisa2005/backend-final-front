bcc86bae6b1174602ca0238953bdea6574f37ca6
Anthony Allois <anthonyallois@gmail.com>
2022-10-11 14:08:36 -0300

    Ajustando Slider

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,23 +1,62 @@
 import React from 'react'
 import data from '../data'
+import Slider from "react-slick";
 
 function Home() {
+
+  const settings = {
+    dots: true,
+    infinite: false,
+    speed: 500,
+    slidesToShow: 3,
+    slidesToScroll: 3,
+    initialSlide: 0,
+    responsive: [
+      {
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 3,
+          slidesToScroll: 3,
+          infinite: true,
+          dots: true
+        }
+      },
+      {
+        breakpoint: 600,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 2,
+          initialSlide: 2
+        }
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1
+        }
+      }
+    ]
+  };
+
   return (
     <div className="max-w-[1240px] m-auto px-4 text-white">
-      <h2 className="text-2xl uppercase py-4">Destacados</h2>
-      <div className="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-6">
-        {data.map( item => {
-          return (
-            <div key={item.id} className="text-white w-full h-[250px] relative hover:cursor-pointer hover:scale-110">
-              <img className="absolute top-0 left-0 w-full h-full object-cover rounded-xl" src={item.imgUrl} alt={item.title} />
-              <div className="absolute bottom-3 left-3 right-3 text-black flex justify-between items-center">
-                <h2 className="text-xl">{item.title}</h2>
-                <p className="text-xl">${item.price}</p>
+      <h2 className="text-2xl uppercase py-4">destacados</h2>
+      {/* <div className="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-6"> */}
+        <Slider {...settings} className="w-[95%] mx-auto px-2 md:px-0">
+          {data.map( item => {
+            return (              
+              <div className="text-white w-full h-[250px] relative hover:cursor-pointer hover:scale-105">
+                <img className="absolute top-0 left-0 w-[95%] h-full object-cover rounded-xl" src={item.imgUrl} alt={item.title} />
+                <div className="px-2 py-1 bg-black rounded-md absolute bottom-3 left-2 w-[90%] mx-auto text-[color:var(--primary-green)] flex justify-between items-center">
+                  <h2 className="text-lg md:text-xl font-bold">{item.title}</h2>
+                  <p className="text-lg md:text-xl font-bold">${item.price}</p>
+                </div>
               </div>
-            </div>
-          );
-        })}
-      </div>
+            );
+          })}
+        </Slider>
+      {/* </div> */}
     </div>
   )
 }
