a38a0273dda11b2bf3f4d9ef4981b846d6b1ac42
Anthony Allois <anthonyallois@gmail.com>
2022-11-02 11:40:54 -0300

    feat: agregando barra lateral STORE

--- src/screens/Store.jsx
+++ src/screens/Store.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import { BiSearchAlt } from 'react-icons/bi'
+import { BiSearchAlt, BiDrink } from 'react-icons/bi'
 
 function Store() {
   return (
@@ -8,6 +8,23 @@ function Store() {
         <div className="w-full  flex justify-center items-center bg-white py-2 px-4 rounded-xl">
           <input className="flex-1 text-black outline-none" type="text" placeholder="Search..."/>
           <BiSearchAlt className="text-gray-500" size={28}/>
+        </div>
+      </div>
+
+      <div className="group absolute left-0 top-[80px] mt-2 w-[60px] overflow-hidden h-[100vh] hover:w-[140px] ease-in duration-300">
+        <div className="w-full h-full p-2 flex flex-col justify-center items-center">
+          <div className="w-full flex items-center justify-start gap-2 py-2 hover:text-[color:var(--primary-green)] hover:cursor-pointer">
+            <BiDrink size={28} />
+            <p className="hidden group-hover:block">Bebidas</p>
+          </div>
+          <div className="w-full flex items-center justify-start gap-2 py-2 hover:text-[color:var(--primary-green)] hover:cursor-pointer">
+            <BiDrink size={28} className=""/>
+            <p className="hidden group-hover:block">Tecnología</p>
+          </div>
+          <div className="w-full flex items-center justify-start gap-2 py-2 hover:text-[color:var(--primary-green)] hover:cursor-pointer">
+            <BiDrink size={28} className=""/>
+            <p className="hidden group-hover:block">Snacks</p>
+          </div>
         </div>
       </div>
 
